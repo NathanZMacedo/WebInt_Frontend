eeed2b9c1667f85c7a0740e5132e85529211bddc
nathanzmacedo <nathanzmacedo@gmail.com>
2024-09-19 17:26:25 -0300

    pequenas alterações

--- src/components/ui/navbar/Navbar.jsx
+++ src/components/ui/navbar/Navbar.jsx
@@ -4,10 +4,10 @@ import './Navbar.css'
 
 function Navbar() {
   return (
-    <div>
+    <nav>
         <Link to='/'>Home</Link>
         <Link to='/products'>Produtos</Link>
-    </div>
+    </nav>
   )
 }
 
